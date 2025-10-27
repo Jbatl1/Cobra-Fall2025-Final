be7b312624a1d6ff23fc7ec4c1f940635bbfc2d8
kwiggins9 <kwiggins9@ggc.edu>
2025-10-27 11:30:26 -0400

    Figured out why it was giving duplicate player methods i had an extra }, everything should be fine

--- src/Model/Entities/Player.java
+++ src/Model/Entities/Player.java
@@ -35,90 +35,90 @@ public class Player extends Entity {
         // TODO: Check if item with name s is in inventory
         return -1;
     }
-}
-public int EquipItemToHands(String s) {
-    // TODO: Equip an item from inventory to hands
-    return 0;
-}
 
-public int EquipItemToToolBelt(String s) {
-    // TODO: Equip an item from inventory to tool belt
-    return 0;
-}
+    public int EquipItemToHands(String s) {
+        // TODO: Equip an item from inventory to hands
+        return 0;
+    }
 
-public int RemoveItemFromHands(String s) {
-    // TODO: Remove equipped item from hands
-    return 0;
-}
+    public int EquipItemToToolBelt(String s) {
+        // TODO: Equip an item from inventory to tool belt
+        return 0;
+    }
 
-public int RemoveItemFromToolBelt(String s) {
-    // TODO: Remove item from tool belt
-    return 0;
-}
+    public int RemoveItemFromHands(String s) {
+        // TODO: Remove equipped item from hands
+        return 0;
+    }
 
-public int dropItem(String s) {
-    // TODO: Drop an item from inventory into the current room
-    return 0;
-}
+    public int RemoveItemFromToolBelt(String s) {
+        // TODO: Remove item from tool belt
+        return 0;
+    }
 
-public int PickupItem(String s) {
-    // TODO: Pick up an item from the current room
-    return 0;
-}
+    public int dropItem(String s) {
+        // TODO: Drop an item from inventory into the current room
+        return 0;
+    }
 
-public int destroyItem(String s) {
-    // TODO: Remove and destroy an item
-    return 0;
-}
+    public int PickupItem(String s) {
+        // TODO: Pick up an item from the current room
+        return 0;
+    }
 
-// ==============================
+    public int destroyItem(String s) {
+        // TODO: Remove and destroy an item
+        return 0;
+    }
+
+    // ==============================
 // Item Usage and Interaction
 // ==============================
-public int useKey(String s) {
-    // TODO: Use a key item to unlock something
-    return 0;
-}
+    public int useKey(String s) {
+        // TODO: Use a key item to unlock something
+        return 0;
+    }
 
-public int inspectItem(String s) {
-    // TODO: Inspect an item for details
-    return 0;
-}
+    public int inspectItem(String s) {
+        // TODO: Inspect an item for details
+        return 0;
+    }
 
-public int attack() {
-    // TODO: Attack using the equipped item
-    return 0;
-}
+    public int attack() {
+        // TODO: Attack using the equipped item
+        return 0;
+    }
 
-public int upgradeItem(String s1, String s2) {
-    // TODO: Upgrade one item using another
-    return 0;
-}
+    public int upgradeItem(String s1, String s2) {
+        // TODO: Upgrade one item using another
+        return 0;
+    }
 
-public int chargeItem(String s1, String s2) {
-    // TODO: Charge or refuel an item
-    return 0;
-}
+    public int chargeItem(String s1, String s2) {
+        // TODO: Charge or refuel an item
+        return 0;
+    }
 
-public int readArtifact(String s) {
-    // TODO: Read an artifact or note
-    return 0;
-}
+    public int readArtifact(String s) {
+        // TODO: Read an artifact or note
+        return 0;
+    }
 
-// ==============================
+    // ==============================
 // Player Actions
 // ==============================
-public int showMap() {
-    // TODO: Display the current map or room layout
-    return 0;
-}
+    public int showMap() {
+        // TODO: Display the current map or room layout
+        return 0;
+    }
 
-public int consume(String s) {
-    // TODO: Consume an item (food, potion, etc.)
-    return 0;
-}
+    public int consume(String s) {
+        // TODO: Consume an item (food, potion, etc.)
+        return 0;
+    }
 
-public int move(String s) {
-    // TODO: Move the player to a new room (e.g., "north", "south", etc.)
-    return 0;
-}
+    public int move(String s) {
+        // TODO: Move the player to a new room (e.g., "north", "south", etc.)
+        return 0;
+    }
 }
