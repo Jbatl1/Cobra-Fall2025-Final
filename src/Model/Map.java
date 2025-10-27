a1a2a54c3608eb15a0a8c390d69bbb2fcbaca3b7
Anita <aphilip4@ggc.edu>
2025-10-27 10:53:17 -0400

    The is my first commit in this repo

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -1,4 +1,19 @@
 package Model;
+import java.sql.Connection;
 
 public class Map {
+    public static void main(String[] args) {
+
+        Connection conn = DatabaseConnection.connect();
+
+        if (conn != null) {
+            System.out.println(" Game can now use the database!");
+        } else {
+            System.out.println(" Could not connect to database.");
+        }
+
+        // Close the connection when done
+        DatabaseConnection.close();
+    }
+
 }
