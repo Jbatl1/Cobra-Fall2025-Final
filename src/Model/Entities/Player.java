183536ddb0b76599e6bc3c821212364c60bb1182
kwiggins9 <kwiggins9@ggc.edu>
2025-10-27 10:27:43 -0400

    Just added a little something to test

--- src/Model/Entities/Player.java
+++ src/Model/Entities/Player.java
@@ -1,4 +1,37 @@
 package Model.Entities;
 
-public class Player extends Entity{
-}
+import java.util.ArrayList;
+import Model.Items.Item;
+import Model.Rooms.Room;
+
+public class Player extends Entity {
+
+    // ==============================
+    // Fields
+    // ==============================
+    private ArrayList<Item> inventory;
+    private ArrayList<Item> toolBelt;
+    private Item equippedItem;
+    private Room currRoom;
+
+    // ==============================
+    // Constructor
+    // ==============================
+    public Player(Room startingRoom) {
+        this.inventory = new ArrayList<>();
+        this.toolBelt = new ArrayList<>();
+        this.currRoom = startingRoom;
+        this.equippedItem = null;
+    }
+
+    // ==============================
+    // Inventory / ToolBelt Management
+    // ==============================
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    private int isInInventory(String s) {
+        // TODO: Check if item with name s is in inventory
+        return -1;
+    }
