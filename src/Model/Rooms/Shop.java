8438f44c81b1756fde789c3f355d690483d4533d
Kai Wiggins <K.Wiggins9@ggc.edu>
2025-10-28 02:38:07 -0400

    Added a couple methods to the raider,restroom,shop,puzzle

--- src/Model/Rooms/Shop.java
+++ src/Model/Rooms/Shop.java
@@ -1,4 +1,40 @@
 package Model.Rooms;
 
+import Model.Entities.Player;
+import Model.Items.Item;
+import java.util.ArrayList;
+
 public class Shop extends Room{
+    private ArrayList<Item> stock;
+
+    public Shop(Strimg id, String name, String description) {
+        super(id, name, description);
+        this.stock = new ArrayList<>();
+    }
+    public void addItemToStock(Item item){
+        stock.add(item);
+    }
+    public void displayStock(){
+        System.out.println("Items for sale:");
+        for (Item item : stock){
+            System.out.println("- " + item.getName());
+        }
+    }
+    public boolean buyItem(Player player, Item item) {
+        if (stock.contains(item)) {
+            player.addToInventory(item);
+            stock.remove(item);
+            System.out.println("You bought " + item.getName() + "!");
+            return true;
+        }
+        System.out.println("Item not found.");
+        return false;
+    }
+
+    public void sellItem(Player player, Item item) {
+        if (player.removeFromInventory(item)) {
+            stock.add(item);
+            System.out.println("You sold " + item.getName() + ".");
+        }
+    }
 }
