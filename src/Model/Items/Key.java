fe719057b4c8f1eb1f180cca32e17f9bfb96bd08
Jbatl1 <cb8876@gmail.com>
2025-10-29 12:24:33 -0400

    added fields, constructors, getters, and setters for all Items

--- src/Model/Items/Key.java
+++ src/Model/Items/Key.java
@@ -1,4 +1,8 @@
 package Model.Items;
 
 public class Key extends Item{
+
+    public Key (String id, String name, String description, String rarity) {
+        super(id, name, description, rarity);
+    }
 }
--- src/Model/Items/Material.java
+++ src/Model/Items/Material.java
@@ -1,4 +1,18 @@
 package Model.Items;
 
 public class Material extends Item {
+    private int damageIncrease;
+
+    public Material(String id, String name, String description, String rarity, int damageIncrease) {
+        super(id, name, description, rarity);
+        this.damageIncrease = damageIncrease;
+    }
+
+    public int getDamageIncrease() {
+        return damageIncrease;
+    }
+
+    public void setDamageIncrease(int damageIncrease) {
+        this.damageIncrease = damageIncrease;
+    }
 }
