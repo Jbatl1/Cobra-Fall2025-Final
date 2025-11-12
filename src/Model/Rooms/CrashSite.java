397f4664d8a297d59e181ddf3de1984eacad3327
aeristran <thmy263@gmail.com>
2025-11-12 10:16:33 -0500

    added game save class

--- src/Model/Rooms/CrashSite.java
+++ src/Model/Rooms/CrashSite.java
@@ -2,49 +2,17 @@ package Model.Rooms;
 
 import Model.Entities.Player;
 import Model.Items.Item;
-import java.sql.*;
-import Model.DatabaseConnection;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 
-public class CrashedShip {
+public class CrashSite {
     private Room crashSite;
     private ArrayList<Item> storage;
 
-    public CrashedShip() {
+    public CrashSite() {
         this.storage=new ArrayList<>();
     }
 
-    public void loadFromDatabase() {
-        try (Connection conn = DatabaseConnection.connect()) {
-
-            if (conn == null) {
-                System.err.println("❌ Could not connect to database.");
-                return;
-            }
-
-            String sql = "SELECT id, name, description FROM rooms WHERE LOWER(type) = 'crash site'";
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                String id = rs.getString("id");
-                crashSite = new Room(id);
-                crashSite.loadFromDatabase();
-
-                System.out.println("🛸 Crashed ship located at crash site room: " + crashSite.getName());
-            } else {
-                System.err.println("⚠️ No crash site room found in database.");
-            }
-
-            rs.close();
-            stmt.close();
-
-        } catch (SQLException e) {
-            System.err.println("Error loading crash site from DB: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 
     public void storeItems(Item item){
         storage.add(item);
