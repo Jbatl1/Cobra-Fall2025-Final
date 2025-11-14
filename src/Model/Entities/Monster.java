36896f26dd6ae812d3da316d582d573b8fef5698
kwiggins9 <kwiggins9@ggc.edu>
2025-11-14 10:45:31 -0500

    Major changes to monster , entity, player

--- src/Model/Entities/Monster.java
+++ src/Model/Entities/Monster.java
@@ -3,25 +3,26 @@ package Model.Entities;
 import Model.Rooms.Room;
 import Model.Items.Item;
 
+/**
+ * Monster entity. No printing inside model.
+ */
 public class Monster extends Entity {
 
-
     // Fields
-    private String monsterID;// Unique ID for the monster
+    private String monsterID;
     private String abilityEffect;
-    private String roomID;
-    private String description;
     private boolean isBoss;
     private Item dropItem;
     private boolean isRaider;
-    private Room currentRoom; // The room this monster belongs to
-
-
+    private Room currentRoom;
 
     // Constructor
-    public Monster(String name, int health, int attackPower, int defense, String monsterID, String abilityEffect, Item dropItem, boolean isBoss, boolean isRaider, Room currentRoom) {
-        super(name, health, attackPower, defense);
+    public Monster(String name, int health, int attackPower, int defense,
+                   String monsterID, String abilityEffect, Item dropItem,
+                   boolean isBoss, boolean isRaider, Room currentRoom) {
 
+        super(name, health, attackPower);
+        this.defense = defense;
 
         this.monsterID = monsterID;
         this.abilityEffect = abilityEffect;
@@ -31,58 +32,30 @@ public class Monster extends Entity {
         this.currentRoom = currentRoom;
     }
 
-    // Getters / Setters
-    public String getMonsterID() {
-        return monsterID;
-    }
+    // Getters
+    public String getMonsterID() { return monsterID; }
+    public String getAbilityEffect() { return abilityEffect; }
+    public Item getDropItem() { return dropItem; }
+    public boolean isBoss() { return isBoss; }
+    public boolean isRaider() { return isRaider; }
+    public Room getCurrentRoom() { return currentRoom; }
 
-    public String getAbilityEffect() {
-        return abilityEffect;
-    }
-
-    public Item getDropItem() {
-        return dropItem;
-    }
-
-    public boolean isBoss() {
-        return isBoss;
-    }
-
-    public boolean isRaider() {
-        return isRaider;
-    }
-
-    public Room getCurrentRoom() {
-        return currentRoom;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-    // Combat
-
-    public void attackPlayer(Player player) {
+    /**
+     * Attack the given player. Returns the damage dealt (after defense).
+     * The View can use the returned value to display messages.
+     */
+    public int attackPlayer(Player player) {
         int damage = Math.max(0, attackPower - player.getDefense());
         player.receiveDamage(damage);
-        System.out.println(name + " attacks " + player.getName() + " for " + damage + " damage!");
+        return damage;
     }
 
-    public void receiveDamage(int amount) {
-        int damageTaken = Math.max(0, amount - defense);
-        health -= damageTaken;
-        if (health < 0) health = 0;
-        System.out.println(name + " takes " + damageTaken + " damage! (" + health + " HP left)");
-        if (health == 0) {
-            System.out.println(name + " has been defeated!");
-        }
-    }
-
-
-    // Rewards / Flee
-
+    /**
+     * If the monster is dead, return its drop item; otherwise return null.
+     * The View or caller handles presentation of this reward.
+     */
     public Item getReward() {
         if (health <= 0 && dropItem != null) {
-            System.out.println(name + " dropped: " + dropItem.getItemName());
             return dropItem;
         }
         return null;
@@ -91,7 +64,7 @@ public class Monster extends Entity {
     public boolean canFlee() {
         return !isBoss;
     }
-
+}
   /*  public void fleeAttempt(Player player) {
         if (!canFlee()) {
             System.out.println("You cannot flee from this boss monster!");
@@ -106,5 +79,5 @@ public class Monster extends Entity {
             }
         }
     }*/
-}
+
 
