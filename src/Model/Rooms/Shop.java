f64f561267e3f427bd9015b54a5ab2cddc46c2a6
Anita <aphilip4@ggc.edu>
2025-11-14 11:17:07 -0500

    changes made for shop and Item and LoadRooms

--- src/Model/Rooms/Shop.java
+++ src/Model/Rooms/Shop.java
@@ -9,10 +9,30 @@ public class Shop extends Room{
 
     //Anita Modified getName() --> getItemName
 
-    public Shop(String roomID, String roomName, String roomDescription, String roomType, String northNavigation, String eastNavigation, String southNavigation, String westNavigation, boolean roomVisited, boolean isRaider, boolean isShop, ArrayList<Item> stock) {
+    private String item_ID;
+    private String name;
+    private String type;
+    private String rarity;
+    private int cost;
+
+  /*  public Shop(String roomID, String roomName, String roomDescription, String roomType, String northNavigation, String eastNavigation, String southNavigation, String westNavigation, boolean roomVisited, boolean isRaider, boolean isShop, ArrayList<Item> stock) {
         super(roomID, roomName, roomDescription, roomType, northNavigation, eastNavigation, southNavigation, westNavigation, roomVisited, isRaider, isShop);
         this.stock = stock;
     }
+*/
+    public Shop(String roomID, String roomName, String roomDescription, String roomType, String northNavigation, String eastNavigation, String southNavigation, String westNavigation, boolean roomVisited, boolean isRaider, boolean isShop, ArrayList<Item> stock, String item_ID, String name, String type, String rarity, int cost) {
+        super(roomID, roomName, roomDescription, roomType, northNavigation, eastNavigation, southNavigation, westNavigation, roomVisited, isRaider, isShop);
+        this.item_ID = item_ID;
+        this.name = name;
+        this.type = type;
+        this.rarity = rarity;
+        this.cost = cost;
+        this.stock = stock;
+    }
+
+
+
+    public int getCost() { return cost; }
 
     public void addItemToStock(Item item){
         stock.add(item);
